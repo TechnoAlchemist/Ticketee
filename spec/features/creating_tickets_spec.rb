984977f917797a894dbca880a48b26e193e3da15
Gabe <glafonta@gmail.com>
2016-04-01 11:35:36 -0400

    File uploads are psersisted across form re-displays

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -41,11 +41,24 @@ RSpec.feature "Users can create new tickets" do
 
   scenario "with an attachment" do
     fill_in "Name", with: "Add documentation for blink tag"
-    fill_in "Description", with: "The blink tag has a speed attribute"
+    fill_in "Description", with: "Add documentation for blink tag"
     attach_file "File", "spec/fixtures/speed.txt"
     click_button "Create Ticket"
 
     expect(page).to have_content "Ticket has been created"
+
+    within("#ticket .attachment") do
+      expect(page).to have_content "speed.txt"
+    end
+  end
+
+  scenario "persisting file uploads across form displays" do
+    attach_file "File", "spec/fixtures/speed.txt"
+    click_button "Create Ticket"
+
+    fill_in "Name", with: "Add documentation for blink tag"
+    fill_in "Description", with: "Add documentation for blink tag"
+    click_button "Create Ticket"
 
     within("#ticket .attachment") do
       expect(page).to have_content "speed.txt"
